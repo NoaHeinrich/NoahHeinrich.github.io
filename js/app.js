476f3140d7698c62b70d9437dff888d79e58703b
Noah Heinrich <noahheinrich@gmail.com>
2016-01-27 14:42:49 -0600

    Add basic jquery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-$(document).foundation();
+$(document).ready(function(){
+  $(".nav-button").on('mouseover', function(event){
+    event.preventDefault();
+    $(this).css("background-color", "#2E9C9C")
+  });
+  $(".nav-button").on("mouseleave", function(event){
+    event.preventDefault();
+    $(this).css("background-color","#77D5D5")
+  });
+});
